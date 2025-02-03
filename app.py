4a7c0276ae03410ca2719399c623fe8601582a05
Mahalakshmi Pilla <159614385+Mahalakshmi-P28@users.noreply.github.com>
2025-02-03 11:20:44 +0530

    Update app.py api streamlit config

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 import google.generativeai as genai
 import os  
-api_key = os.getenv("API_KEY")
+api_key = st.secrets["google"]["api_key"]
 
 genai.configure(api_key=api_key)
 
